ff31992b8bff8c3afef6804b573e772f5608d08b
LuciLua <luci.lua81@gmail.com>
2022-09-29 13:54:35 -0300

    :star2: github api integration and style change

--- frontend/src/components/Dashboard/Dashboard.tsx
+++ frontend/src/components/Dashboard/Dashboard.tsx
@@ -3,12 +3,30 @@ import { BiArrowBack, BiUser } from "react-icons/bi"
 import { usePanel } from "../../hooks/usePanel"
 import { useUser } from "../../hooks/useUser"
 import Head from "next/head"
-import { AiFillCode } from 'react-icons/ai'
+import { AiFillCode, AiOutlineTwitter } from 'react-icons/ai'
+
+import { useEffect, useState } from "react"
+
+import axios from 'axios'
+import Image from "next/image"
 
 function Dashboard() {
 
-    const { panel, setPanel } = usePanel()
-    const { user, setUser } = useUser()
+    const { setPanel } = usePanel()
+    const { user } = useUser()
+
+    const [githubUser, setGithubUser] = useState<any>()
+
+    async function searchUserGitHub() {
+        const url = `https://api.github.com/users/${user.name}`
+        const json = await axios.get(url)
+        const data = await json.data
+        setGithubUser(data)
+    }
+
+    useEffect(() => {
+        searchUserGitHub()
+    }, [])
 
 
     return (
@@ -22,9 +40,17 @@ function Dashboard() {
             <div className={styles.content}>
                 <div className={styles.cardContainer}>
                     <div className={styles.card}>
-                        <span className={styles.logo}><BiUser /></span>
+                        <span className={styles.image}>
+                            <Image src={githubUser ? githubUser.avatar_url : '/'} layout='fill' />
+                        </span>
                         <span className={styles.text}>{user.name}</span>
                         <span className={styles.email}>{user.email}</span>
+                    </div>
+                    <div className={styles.card}>
+                        <span className={styles.others}>{githubUser ? githubUser.bio : ''}</span>
+                        <span className={styles.others}>{githubUser ? githubUser.location : ''}</span>
+                        <span className={styles.others}><AiOutlineTwitter />{githubUser ? githubUser.twitter_username : ''}</span>
+                        <span className={styles.others}>{githubUser ? githubUser.blog : ''}</span>
                     </div>
                 </div>
                 <div className={styles.contentUser}>
@@ -32,11 +58,30 @@ function Dashboard() {
                         <div className={styles.posts}>
                             <div className={styles.writePost}>
                                 <textarea placeholder="Write a post here..." name="" id=""></textarea>
-                                <button>Send</button>
+                                <button onClick={() => searchUserGitHub()}>Send</button>
                             </div>
                             <div className={styles.postsContainer}>
                                 <div className={styles.post}>
-                                    aaa
+                                    <h1>
+                                        Title
+                                    </h1>
+                                    <h2>
+                                        subtitle
+                                    </h2>
+                                    <p>
+                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod voluptatibus possimus numquam enim vitae. Ad, maiores corrupti, neque odit natus suscipit dignissimos recusandae tempora velit mollitia omnis rerum voluptatem! Beatae.
+                                    </p>
+                                </div>
+                                <div className={styles.post}>
+                                    <h1>
+                                        Title
+                                    </h1>
+                                    <h2>
+                                        subtitle
+                                    </h2>
+                                    <p>
+                                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod voluptatibus possimus numquam enim vitae. Ad, maiores corrupti, neque odit natus suscipit dignissimos recusandae tempora velit mollitia omnis rerum voluptatem! Beatae.
+                                    </p>
                                 </div>
                             </div>
                         </div>
